caa4130a55e4e6aa5870c2ca571522b888aaf544
polyacetal <yotyan988@gmail.com>
2022-04-18 16:28:29 +0900

    add prov.sh

--- startUp.sh
+++ startUp.sh
@@ -6,6 +6,6 @@ read -sp "Enter your password :" password
 export http_proxy="http://${username}:${password}@157.114.16.93:8080"
 export https_proxy="http://${username}:${password}@157.114.16.93:8080"
 
-curl https://raw.githubusercontent.com/KNCT-KPC/dotfiles-setupscripts/master/start.sh | bash
+curl https://raw.githubusercontent.com/KNCT-KPC/dotfiles-setupscripts/master/prov.sh | bash
 
-apt install build-essential
+sudo -E apt install build-essential
